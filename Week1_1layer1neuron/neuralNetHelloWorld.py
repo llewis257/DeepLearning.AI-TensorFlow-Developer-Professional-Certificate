871a744f8104b9a9f2d752e4fab69bc356308e62
llewis257 <30831853+llewis257@users.noreply.github.com>
2024-02-22 09:35:32 +0100

    'mean absolute vs squared, 1 layer vs 2 layers'

--- Week1_1layer1neuron/neuralNetHelloWorld.py
+++ Week1_1layer1neuron/neuralNetHelloWorld.py
@@ -4,13 +4,15 @@ from tensorflow import keras
 
 def neural_net(pred: int) -> int:
     layers = [
-        tf.keras.layers.Dense(units=1, input_shape=[1])
+        tf.keras.layers.Dense(units=1, input_shape=[1]),
+        tf.keras.layers.Dense(units=2),
+        tf.keras.layers.Dense(units=1)
     ] 
     # defining the layers of the model 
     # -> 1 index = 1 layer
     # -> units=1  = 1 neuron
     model = tf.keras.Sequential(layers=layers)
-    model.compile(optimizer= 'sgd', loss = 'mean_squared_error')
+    model.compile(optimizer= 'sgd', loss = 'mean_absolute_error')
     print(type(model))
 
     # input training data
@@ -21,7 +23,7 @@ def neural_net(pred: int) -> int:
     #training
     model.fit(x,y,epochs=100)
     prediction = model.predict([pred])
-    return int(round(float(prediction), ndigits=0))
+    return prediction
 
 if __name__ == '__main__':
-    print(neural_net(12))
+    print(neural_net(101))
